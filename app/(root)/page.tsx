08f7506b8f3430997a2b7c57938849514d99f1cf
Meet Batra <meetbatra56@gmail.com>
2025-10-23 01:14:53 +0530

    Home page startup cards layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,21 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({ searchParams } : { searchParams: Promise<{ query?: string}>}) {
   const query = (await searchParams).query;
+
+  const posts = [
+    {
+    _createdAt: new Date(),
+    views: 55,
+    author: { _id: 1, name: "Meet"},
+    _id: 1,
+    description: "This is a description", 
+    image: "https://images.unsplash.com/photo-1634912314704-c646c586b131?g=808w=2940Sauto=format&fit=crop&ixlib=rb-4.038іxid=MЗwхМjA3fDB8МHхwaG90by1wYWdlfHх8fGVufDВ8fHх8fA%3D%3D",
+    category: "Robots", 
+    title: "We Robots"
+    }
+  ]
 
   return (
     <>
@@ -23,6 +37,16 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : 'All Startups'}
         </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post : StartupCardType, index : number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <></>
+          )}
+        </ul>
       </section>
     </>
   )

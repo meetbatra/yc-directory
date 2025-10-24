e8eef3ba020800d25af7779f17de9991c4534392
Meet Batra <meetbatra56@gmail.com>
2025-10-24 13:00:08 +0530

    Live API integration with sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,11 @@
 import StartupCard, {StartupCardType} from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import {sanityFetch, SanityLive} from "@/sanity/lib/live";
 
 export default async function Home({ searchParams } : { searchParams: Promise<{ query?: string}>}) {
   const query = (await searchParams).query;
-
-  const posts = await client.fetch(STARTUPS_QUERY);
-
-  // const posts = [
-  //   {
-  //   _createdAt: new Date(),
-  //   views: 55,
-  //   author: { _id: 1, name: "Meet"},
-  //   _id: 1,
-  //   description: "This is a description", 
-  //   image: "https://images.unsplash.com/photo-1634912314704-c646c586b131?g=808w=2940Sauto=format&fit=crop&ixlib=rb-4.038іxid=MЗwхМjA3fDB8МHхwaG90by1wYWdlfHх8fGVufDВ8fHх8fA%3D%3D",
-  //   category: "Robots", 
-  //   title: "We Robots"
-  //   }
-  // ]
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -44,7 +30,7 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
 
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post : StartupCardType, index : number) => (
+            posts.map((post : StartupCardType) => (
               <StartupCard key={post?._id} post={post} />
             ))
           ) : (
@@ -52,6 +38,8 @@ export default async function Home({ searchParams } : { searchParams: Promise<{ 
           )}
         </ul>
       </section>
+
+      <SanityLive />
     </>
   )
 }
